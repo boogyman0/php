515d421ac0efe64f0de1d633b1d2af4beb1d7e20
boogyman0 <boogyman998@gmail.com>
2025-06-20 16:04:11 +0700

    Add files via upload

--- owen.php
+++ owen.php
@@ -1,11 +1,13 @@
 <?php
-$fruits1  = ['a'=>"apple",'b'=>"bee",'c'=>"cat",'d'=>"dog"];
+$fruits1  = ['a'=>"Apple",'b'=>"Bee",'c'=>"cat",'d'=>"Dog"];
 $fruits2  = ['b' => 'boy','d'=> 'dog'];
 foreach ($fruits1 as $a => $value){
     echo "[$a] : $value<br>";
+
 }
 $result = array_merge($fruits1,$fruits2);
 foreach ($result as $a => $value){
     echo "[$a] : $value<br>";
+
 }
 ?>
